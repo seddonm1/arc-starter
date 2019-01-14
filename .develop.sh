31124dc7dd66de6c7c74f64842efab095fea1e2a
a139834 <mseddon@agl.com.au>
2019-01-14 14:04:56 +1100

    Update .develop.sh

--- .develop.sh
+++ .develop.sh
@@ -1,8 +1,11 @@
 docker run \
 --rm \
+-v /tmp/arc-jupyter:/local \
 -v $(pwd):/home/jovyan \
 -e "ETL_JOB_ROOT=/home/jovyan" \
 -e "SPARK_OPTS='--driver-memory=4G'" \
+-e "SPARK_LOCAL_DIRS=/local" \
+-e "SPARK_WORKER_DIR=/local" \
 -p 4040:4040 \
 -p 8888:8888 \
-seddonm1/arc-jupyter:0.0.1
+seddonm1/arc-jupyter:0.0.2
